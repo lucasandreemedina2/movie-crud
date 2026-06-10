1b7ab12815629d0b322b7c6a5c6c246a21f053ad
lucasandreemedina2 <lucasandreemedina2@gmail.com>
2026-06-09 21:48:37 -0300

    Buscador funcionando

--- movie-crud/src/components/Navbar.jsx
+++ movie-crud/src/components/Navbar.jsx
@@ -17,23 +17,4 @@ function Navbar({ search, setSearch }) {
   );
 }
 
-export default Navbar;function Navbar({ search, setSearch }) {
-  return (
-    <nav className="navbar">
-      <h2>🎬 Movie CRUD</h2>
-
-      <div className="search-box">
-        <input
-          type="text"
-          placeholder="Buscar película..."
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-        />
-
-        <button>Buscar</button>
-      </div>
-    </nav>
-  );
-}
-
 export default Navbar;
